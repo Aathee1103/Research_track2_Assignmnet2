ab3c3586c45edac683351a541930fe379b68be54
Aathee1103 <80621864+Aathee1103@users.noreply.github.com>
2022-02-13 17:47:31 +0000

    Delete rt2_sphinx directory

--- rt2_sphinx/_build/html/search/functions_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['go_5fstraight_5fahead_25',['go_straight_ahead',['../namespacego__to__point.html#ac8579665c0fbf665f734476554fac37d',1,'go_to_point']]]
-];
--- rt2_sphinx/docs/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['act_5fs_0',['act_s',['../go__to__point_8py.html#a64583fad586a7212c40613ec1c3e7c69',1,'go_to_point']]]
-];
